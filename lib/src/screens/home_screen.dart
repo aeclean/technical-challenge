e97b6a9fe118e54b922ce5324d93a86040ff4c03
Aecio Lima <aeclean@gmail.com>
2025-02-14 16:24:56 -0300

    implements todo UI

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import "package:faithwave_app/src/features/auth/cubits/auth_cubit.dart";
+import "package:faithwave_app/src/features/todos/cubits/todos_cubit.dart";
+import "package:faithwave_app/src/models/todo.dart";
 import "package:faithwave_app/src/router/router.dart";
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
 import "package:go_router/go_router.dart";
 
@@ -34,12 +37,119 @@ class HomeView extends StatelessWidget {
         ],
       ),
       body: SafeArea(
-        child: Center(),
+        child: BlocBuilder<TodoCubit, TodoState>(
+          bloc: context.read<TodoCubit>()..fetchTodos(),
+          builder: (context, state) {
+            return BlocBuilder<TodoCubit, TodoState>(
+              builder: (context, state) {
+                if (state.isLoading) {
+                  return const Center(child: CircularProgressIndicator(),);
+                } else {
+                  return state.todos.mapOrElse(
+                    (results) => Padding(
+                      padding: const EdgeInsets.all(22),
+                      child: ListView.builder(
+                        itemCount: results.length,
+                        itemBuilder: (context, index) => ListItem(results[index]),
+                      ),
+                    ),
+                    () => const EmptyView(),
+                  );
+                }
+              },
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => null,
+        onPressed: () => showDialogView(context),
         child: const Icon(Icons.add_outlined),
       ),
+    );
+  }
+
+  void showDialogView(BuildContext context) {
+    final controller = TextEditingController();
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: const EdgeInsets.all(22),
+          child: Column(
+            children: [
+              const Text(
+                "Add a Todo",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 22,),
+              TextField(
+                controller: controller,
+                decoration: const InputDecoration(
+                  hintText: "Insert a title",
+                ),
+              ),
+              const Expanded(child: const Center()),
+              SizedBox(
+                width: double.maxFinite,
+                child: OutlinedButton(
+                  onPressed: () {
+                    if (controller.text.isNotEmpty) {
+                      context.read<TodoCubit>().addTodo(
+                        title: controller.text,
+                        isChecked: false,
+                      );
+                      context.pop();
+                    }
+                  },
+                  child: const Text("Add"),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class ListItem extends StatelessWidget {
+  ListItem(this.todo, {super.key});
+
+  Todo todo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Text(todo.title),
+        ),
+        Checkbox(
+          value: todo.isChecked,
+          onChanged: (_) {
+            context.read<TodoCubit>().toggleComplete(todo: todo);
+          },
+        ),
+        IconButton(
+          onPressed: () => context.read<TodoCubit>().deleteTodo(todo: todo),
+          icon: const Icon(Icons.delete),
+        ),
+      ],
+    );
+  }
+}
+
+class EmptyView extends StatelessWidget {
+  const EmptyView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("No data yet."),
     );
   }
 }
